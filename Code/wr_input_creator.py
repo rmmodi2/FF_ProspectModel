7b949cd0d7e48f38317b615ab15d7619ea630781
rmmodi2 <ronakmodi.ca@gmail.com>
2020-04-20 14:49:35 -0700

    Finished Linear Model for WR, moving on to decision trees

--- Code/wr_input_creator.py
+++ Code/wr_input_creator.py
@@ -4,7 +4,7 @@ from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import KFold
 import helpers
 
-# import the data from the prospect database, take out prospects who have not played in the nfl yet, take out UDFAs.
+# import the data from the prospect database, take out prospects who have not played in the nfl yet, take out UDFAs, make sure any NA in PPR PPG become 0.
 
 wr_data = pd.read_excel("/Users/ronakmodi/FF_ProspectModel/Data/pahowdy_prospect_database.xlsx", sheet_name = "WR", header=[0,1], na_values=["-"])
 print("DEBUG: ###################################")
@@ -13,13 +13,7 @@ print("DEBUG: ###################################")
 wr_data = wr_data[wr_data["Unnamed: 6_level_0"]["Draft Year"]!="2021 PROSPECT"]
 wr_data = wr_data[wr_data["Unnamed: 6_level_0"]["Draft Year"]!="2020 PROSPECT"]
 wr_data = wr_data[wr_data["Unnamed: 4_level_0"]["DP"]!="UDFA"]
-
-# import the data for players final year to get the end date for player's careers
-
-players_final_year = pd.read_excel("/Users/ronakmodi/FF_ProspectModel/Data/player_final_year.xlsx", sheet_name = "main")
-print("DEBUG: ###################################")
-print(list(players_final_year.columns))
-print("DEBUG: ###################################")
+wr_data[("NFL Career Marks since 2000","AVG PPG (PPR) Season 1-3")].fillna(value=0, inplace=True)
 
 
 """
@@ -38,41 +32,13 @@ TODO: left out: Height Adjusted Speed Score, BEST MS Yards, Total College Domina
 # Create X and Y
 
 
-# Start with the "independent variables - model stuff and draft capital"
+# Start with the "independent variables - model stuff and draft capital" , last one is the dependent variable - Avg PPG first 3 years in the NFL.
 
-filtered_cols = wr_data[[('Unnamed: 0_level_0', 'Name'), ('Unnamed: 1_level_0', 'School'), ("Unnamed: 4_level_0",'DP'), ("Breakout Ages", ">20%"), ("Breakout Ages", ">30%"), 
+filtered_cols = wr_data[[('Unnamed: 0_level_0', 'Name'), ('Unnamed: 1_level_0', 'School'), ("Unnamed: 4_level_0",'DP'), ("Unnamed: 6_level_0",'Draft Year'), ("Unnamed: 7_level_0",'Age IN DRAFT YEAR (9/1/dy)'), ("Breakout Ages", ">20%"), ("Breakout Ages", ">30%"), 
 ("RecYds/TmPatt", "BEST"), ("Dominator", "BEST"), ("MS Yards", "BEST"), ("Context Scores", "PPG Above conference expectation (Last Year)"), ("Context Scores", "TeamMate Score"), 
-("Context Scores", "Last S/EX"), ("Combine", "WaSS"), ("Combine", "HaSS"), ("Combine", "BMI")]]
-draft_capital_only = wr_data[[('Unnamed: 0_level_0', 'Name'), ("Unnamed: 4_level_0",'DP')]]
+("Context Scores", "Last S/EX"), ("Combine", "WaSS"), ("Combine", "HaSS"), ("Combine", "BMI"), ("NFL Career Marks since 2000","AVG PPG (PPR) Season 1-3"), ("Total Counting Stats", "REC/g"), ("Total Counting Stats", "Years Played")]]
+draft_capital_only = wr_data[[('Unnamed: 0_level_0', 'Name'), ("Unnamed: 4_level_0",'DP'), ("NFL Career Marks since 2000","AVG PPG (PPR) Season 1-3")]]
 
-# Dependent Variable is the score
-
-finish_cols = wr_data[[('Unnamed: 0_level_0', 'Name'), ("Unnamed: 4_level_0",'DP'), ("Unnamed: 6_level_0",'Draft Year'), ("NFL Career Marks since 2000", "# of top 5  finishes"), ("NFL Career Marks since 2000", "# of top  12 finishes"), 
-("NFL Career Marks since 2000", "# of top  24 finishes"), ("NFL Career Marks since 2000", "# of top  36 finishes")]]
-finish_cols.fillna("0", inplace=True)
-
-# we are going to create a dataframe with the name and score
-
-name_and_score = []
-
-for index,row in finish_cols.iterrows():
-	real_top_36 = int(row["NFL Career Marks since 2000"]["# of top  36 finishes"]) - int(row["NFL Career Marks since 2000"]["# of top  24 finishes"])
-	real_top_24 = int(row["NFL Career Marks since 2000"]["# of top  24 finishes"]) - int(row["NFL Career Marks since 2000"]["# of top  12 finishes"])
-	real_top_12 = int(row["NFL Career Marks since 2000"]["# of top  12 finishes"]) - int(row["NFL Career Marks since 2000"]["# of top 5  finishes"])
-	real_top_5 = int(row["NFL Career Marks since 2000"]["# of top 5  finishes"])
-	score = real_top_36 + 2*real_top_24 + 4*real_top_12 + 8*real_top_5
-	print("DEBUG: score for %s is %s" % (row['Unnamed: 0_level_0']['Name'], score))
-	total_years = helpers.get_total_years(row, players_final_year)
-	score = score/total_years
-	name_and_score.append([row['Unnamed: 0_level_0']['Name'], score])
-
-name_and_score = pd.DataFrame(name_and_score, columns=["Name", "Score"])
-
-# sort by alphabetical names
-
-print("DEBUG: ###################################")
-print(name_and_score)
-print("DEBUG: ###################################")
 
 # replace the NA values from filtered columns with what we want
 # sort by alphabetical names
@@ -82,12 +48,10 @@ filtered_cols[("Breakout Ages", ">30%")].fillna(value=35, inplace=True)
 filtered_cols.fillna(filtered_cols.mean(), inplace=True)
 
  # sort everything by alphabetical name and save to excel files, we will worry about joining manually.
-name_and_score.sort_values(by=["Name"], inplace=True)
 filtered_cols.sort_values(by=[('Unnamed: 0_level_0', 'Name')], inplace=True)
 draft_capital_only.sort_values(by=[('Unnamed: 0_level_0', 'Name')], inplace=True)
 
 
-name_and_score.to_excel("/Users/ronakmodi/FF_ProspectModel/Data/wr_name_and_score.xlsx")
 filtered_cols.to_excel("/Users/ronakmodi/FF_ProspectModel/Data/wr_filtered_columns.xlsx")
 draft_capital_only.to_excel("/Users/ronakmodi/FF_ProspectModel/Data/wr_draft_capital_only.xlsx")
 
--- Code/wr_model.py
+++ Code/wr_model.py
@@ -4,7 +4,8 @@ from sklearn import datasets, linear_model, preprocessing
 from sklearn.linear_model import PoissonRegressor 
 from sklearn.metrics import mean_squared_error, r2_score, mean_poisson_deviance, mean_absolute_error
 from sklearn.model_selection import KFold, train_test_split
-from sklearn.preprocessing import normalize
+from sklearn.compose import ColumnTransformer 
+from sklearn.preprocessing import normalize, Normalizer, StandardScaler
 import helpers
 import matplotlib.pyplot as plt
 
@@ -16,33 +17,54 @@ draft_capital_only = pd.read_excel("/Users/ronakmodi/FF_ProspectModel/Data/wr_dr
 print("DEBUG: Number of players in our model sample: %s" % filtered_columns.shape[0])
 print("DEBUG: Number of players in our draft capital sample: %s" % draft_capital_only.shape[0])
 
-# take the log of draft capital
+# drop the columns we don't want in our model, preprocess filtered_columns
 
-filtered_columns.loc[:, ("Unnamed: 4_level_0", 'DP')] = np.log(filtered_columns["Unnamed: 4_level_0"]['DP'])
-draft_capital_only.loc[:, ("Unnamed: 4_level_0", 'DP')] = np.log(draft_capital_only["Unnamed: 4_level_0"]['DP'])
-
-#drop the columns we don't want in our model
-
-filtered_columns = filtered_columns.drop(columns=[('Unnamed: 0_level_0', 'Unnamed: 0_level_1'), ('Unnamed: 0_level_0', 'Name'), ('Unnamed: 1_level_0', 'School'), ('score_hdr', 'Score'), ('Breakout Ages', '>20%'),
-	('Dominator', 'BEST'), ('Context Scores', 'Last S/EX'), ('Combine', 'WaSS'), ('Combine', 'HaSS'), ('MS Yards', 'BEST')])
+model_columns = filtered_columns.drop(columns=[('Unnamed: 0_level_0', 'Unnamed: 0_level_1'), ('Unnamed: 0_level_0', 'Name'), ('Unnamed: 1_level_0', 'School'), ("NFL Career Marks since 2000","AVG PPG (PPR) Season 1-3"), ('Breakout Ages', '>20%'), 
+	('Dominator', 'BEST'), ('Context Scores', 'PPG Above conference expectation (Last Year)'), ('Context Scores', 'TeamMate Score'), ('MS Yards', 'BEST'), ('Combine', 'HaSS'), ("Unnamed: 6_level_0",'Draft Year'), ('Context Scores', 'Last S/EX'), 
+	('Total Counting Stats', 'REC/g'), ('Total Counting Stats', 'Years Played')])
 
 # now we have to split it into an X array and a Y array
 
-X_model = np.array(filtered_columns.values.tolist())
+X_model = np.array(model_columns.values.tolist())
 X_draftcap = np.array(draft_capital_only[[('Unnamed: 4_level_0', 'DP')]].values.tolist())
-Y = np.array(draft_capital_only[[('score_hdr','Score')]].values.tolist())
-n_splits = 10
+Y = np.array(draft_capital_only[[("NFL Career Marks since 2000","AVG PPG (PPR) Season 1-3")]].values.tolist())
 
+normalize(X_model,copy=False)
+
+n_splits = 10
 kf = KFold(n_splits=n_splits,shuffle=True)
 
 # run our best models for just draft capital and our model.
+model = linear_model.LinearRegression()
+
+model_mae = 0
+model_mse = 0
+model_r2 = 0
+max_r2 = -19238409327423
+best_coef = []
+i=0
+for train, test in kf.split(X_model, Y):
+	model.fit(X_model[train], Y[train])
+	y_pred = model.predict(X_model[test])
+	model_mse += mean_squared_error(Y[test], y_pred)
+	model_mae += mean_absolute_error(Y[test], y_pred)
+	tmp_r2 = r2_score(Y[test], y_pred)
+	if tmp_r2 > max_r2:
+		max_r2=tmp_r2
+		best_coef=model.coef_
+	model_r2+=tmp_r2
+	i+=1
+
+print("DEBUG: LINEAR Average R2 value for the model was %s" % (model_r2/i))
+print("DEBUG: LINEAR BEST R2 value for the model was %s" % max_r2)
+# print("DEBUG: LINEAR Average MAE value for the model was %s" % (model_mae/i))
+# print("DEBUG: LINEAR Average MSE value for the model was %s\n" % (model_mse/i))
 
 draft_capital_only = linear_model.LinearRegression()
 
 draft_cap_mse = 0
 draft_cap_mae = 0
 draft_cap_r2 = 0
-draft_cap_d2 = 0
 i=0
 for train, test in kf.split(X_draftcap, Y):
 	draft_capital_only.fit(X_draftcap[train], Y[train])
@@ -50,56 +72,67 @@ for train, test in kf.split(X_draftcap, Y):
 	draft_cap_mse += mean_squared_error(Y[test], y_pred)
 	draft_cap_mae += mean_absolute_error(Y[test], y_pred)
 	draft_cap_r2 += r2_score(Y[test], y_pred)
-	mask = y_pred > 0
-	draft_cap_d2 += mean_poisson_deviance(Y[test][mask], y_pred[mask])
 	i+=1
 
 print("DEBUG: LINEAR Average R2 value for just draft capital was %s" % (draft_cap_r2/i))
-print("DEBUG: LINEAR Average D2 value for just draft capital was %s" % (draft_cap_d2/i))
-print("DEBUG: LINEAR Average MSE value for just draft capital %s" % (draft_cap_mse/i))
-print("DEBUG: LINEAR Average MAE value for just draft capital %s" % (draft_cap_mae/i))
-
-model = PoissonRegressor(alpha=1/X_model.shape[0], max_iter=1000)
-
-model_mse = 0
-model_mae = 0
-model_d2 = 0
-model_r2 = 0
-i=0
-for train, test in kf.split(X_model, Y):
-	model.fit(X_model[train], Y[train].ravel())
-	y_pred = model.predict(X_model[test])
-	model_mse += mean_squared_error(Y[test], y_pred)
-	model_mae += mean_absolute_error(Y[test], y_pred)
-	model_r2 += r2_score(Y[test], y_pred)
-	model_d2 += mean_poisson_deviance(Y[test], y_pred)
-	i+=1
-
-print("DEBUG: POISSON Average R2 value for the model was %s" % (model_r2/i))
-print("DEBUG: POISSON Average D2 value for the model was %s" % (model_d2/i))
-print("DEBUG: POISSON Average MSE value for the model was %s" % (model_mse/i))
-print("DEBUG: POISSON Average MAE value for the model was %s" % (model_mae/i))
+# print("DEBUG: LINEAR Average MAE value for just draft capital %s" % (draft_cap_mae/i))
+# print("DEBUG: LINEAR Average MSE value for just draft capital %s\n" % (draft_cap_mse/i))
 
 # let's see what coefficients our model came up with
-feature_name_weight = dict(zip(filtered_columns.columns, model.coef_))
-print("DEBUG: weights for all of our features: %s" % feature_name_weight)
 
-# let's graph it
+print("Features %s" % list(model_columns))
+print("Weights %s" % list(best_coef))
 
-# model_x_train, model_x_test, model_draft_train, model_draft_test, model_y_train, model_y_test = train_test_split(X_model, X_draftcap, Y, test_size=.1, shuffle=True)
+# now let's see how we did on one class itself
 
-# model.fit(model_x_train, model_y_train.ravel())
-# draft_capital_only.fit(model_draft_train, model_y_train)
+train = filtered_columns[filtered_columns["Unnamed: 6_level_0"]["Draft Year"] != 2014]
+test = filtered_columns[filtered_columns["Unnamed: 6_level_0"]["Draft Year"] == 2014]
 
-# model_pred = model.predict(model_x_test)
-# draft_cap_pred = draft_capital_only.predict(model_draft_test)
+train_columns = train.drop(columns=[('Unnamed: 0_level_0', 'Unnamed: 0_level_1'), ('Unnamed: 0_level_0', 'Name'), ('Unnamed: 1_level_0', 'School'), ("NFL Career Marks since 2000","AVG PPG (PPR) Season 1-3"),('Breakout Ages', '>20%'), 
+	('Dominator', 'BEST'), ('Context Scores', 'PPG Above conference expectation (Last Year)'), ('Context Scores', 'TeamMate Score'), ('MS Yards', 'BEST'), ('Combine', 'HaSS'), ("Unnamed: 6_level_0",'Draft Year'), ('Context Scores', 'Last S/EX'), 
+	('Total Counting Stats', 'REC/g'), ('Total Counting Stats', 'Years Played')])
+X_train = np.array(train_columns.values.tolist())
+test_columns = test.drop(columns=[('Unnamed: 0_level_0', 'Unnamed: 0_level_1'), ('Unnamed: 0_level_0', 'Name'), ('Unnamed: 1_level_0', 'School'), ("NFL Career Marks since 2000","AVG PPG (PPR) Season 1-3"),('Breakout Ages', '>20%'), 
+	('Dominator', 'BEST'), ('Context Scores', 'PPG Above conference expectation (Last Year)'), ('Context Scores', 'TeamMate Score'), ('MS Yards', 'BEST'), ('Combine', 'HaSS'), ("Unnamed: 6_level_0",'Draft Year'), ('Context Scores', 'Last S/EX'), 
+	('Total Counting Stats', 'REC/g'), ('Total Counting Stats', 'Years Played')])
+X_test = np.array(test_columns.values.tolist())
 
-# plt.plot(range(len(model_pred)), model_pred, "g--", range(len(model_pred)), model_y_test, "r^", range(len(model_pred)), draft_cap_pred, "b--")
-# plt.xlabel("index of observation")
-# plt.ylabel("score - model in green, draft capital in blue, actual in red")
-# plt.title("Predicting scores of Wide Receiver Prospects")
-# plt.grid(True)
-# plt.show()
+Y_train = np.array(train[[("NFL Career Marks since 2000","AVG PPG (PPR) Season 1-3")]].values.tolist())
+Y_test = np.array(test[[("NFL Career Marks since 2000","AVG PPG (PPR) Season 1-3")]].values.tolist())
+
+normalize(X_train,copy=False)
+normalize(X_test,copy=False)
+
+model = linear_model.LinearRegression()
+model.fit(X_train, Y_train)
+Y_pred = model.predict(X_test)
+
+print("DEBUG: R^2 for one draft class is %s" % r2_score(Y_test,Y_pred))
+
+result = pd.DataFrame({"Name": test[(('Unnamed: 0_level_0', 'Name'))], "Predicted PPR PPG Years 1-3": Y_pred.ravel(), "Actual PPR ...": Y_test.ravel()})
+result.sort_values(by="Predicted PPR PPG Years 1-3",inplace=True,ascending=False)
+print(result)
+
+
+# let's graph the model / draft capital / actual for a random train/test split
+
+model_x_train, model_x_test, model_draft_train, model_draft_test, model_y_train, model_y_test = train_test_split(X_model, X_draftcap, Y, test_size=.1, shuffle=True)
+
+normalize(model_x_train,copy=False)
+normalize(model_x_test,copy=False)
+
+model.fit(model_x_train, model_y_train.ravel())
+draft_capital_only.fit(model_draft_train, model_y_train)
+
+model_pred = model.predict(model_x_test)
+draft_cap_pred = draft_capital_only.predict(model_draft_test)
+
+plt.plot(range(len(model_pred)), model_pred, "g--", range(len(model_pred)), model_y_test, "r^", range(len(model_pred)), draft_cap_pred, "bo")
+plt.xlabel("index of observation")
+plt.ylabel("PPG - model in green, draft capital in blue, actual in red")
+plt.title("Predicting PPR PPG of Wide Receiver Prospects (Years 1-3)")
+plt.grid(True)
+plt.show()
 
 
 
